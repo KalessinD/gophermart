f994ba8f5213e9999453b5ff41066a704a8d4071
dchernenko <d.chernenko@corp.mail.ru>
2026-05-02 01:33:17 +0300

    refactoring

--- internal/models/accrual_response.go
+++ internal/models/accrual_response.go
@@ -1,5 +1,9 @@
 package models
 
 type (
-	AccrualResponse struct{}
+	AccrualResponse struct {
+		Order   string `json:"order"`
+		Status  string `json:"status"`
+		Accrual string `json:"accrual"` // будем потом переводить в целые копейки
+	}
 )
