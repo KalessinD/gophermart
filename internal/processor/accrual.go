34c3b25eda5cb8f4dc934b08484465b774877a41
dchernenko <d.chernenko@corp.mail.ru>
2026-05-01 23:50:46 +0300

    adding dispatcher

--- internal/processor/accrual.go
+++ internal/processor/accrual.go
@@ -19,7 +19,7 @@ type (
 
 	// Сотрудник-одиночка для обработки заказов
 	Worker struct {
-		ch          <-chan *Task
+		tasksCh     <-chan *Task
 		postProcess func(*Task) error
 		id          int
 		log         *zap.Logger
@@ -34,12 +34,14 @@ type (
 
 	// Объединение сотрудников в коллектив для обработки заказов сообща
 	WorkerPool struct {
-		workers []WorkerInterface
-		wg      sync.WaitGroup
-		mx      sync.Mutex
-		ch      chan *Task
-		cancel  context.CancelFunc
-		log     *zap.Logger
+		workers      []WorkerInterface
+		wg           sync.WaitGroup
+		mx           sync.Mutex
+		workerCh     chan *Task
+		cancel       context.CancelFunc
+		log          *zap.Logger
+		pendingTasks []*Task
+		hasTasks     chan struct{}
 	}
 
 	// Интерфейс для управления коллективом сотрудников
@@ -61,7 +63,7 @@ func NewTask(orderID, userID string) *Task {
 // Сотворение сотрудника
 func NewWorker(id int, ch <-chan *Task, log *zap.Logger, action func(*Task) error) WorkerInterface {
 	return &Worker{
-		ch:          ch,
+		tasksCh:     ch,
 		postProcess: action,
 		id:          id,
 		log:         log,
@@ -71,15 +73,17 @@ func NewWorker(id int, ch <-chan *Task, log *zap.Logger, action func(*Task) erro
 // Метод создания рабочего коллектива
 func NewWorkerPool(poolSize, bufSize int, log *zap.Logger, action func(*Task) error) WorkerPoolInterface {
 	pool := &WorkerPool{
-		workers: make([]WorkerInterface, poolSize),
-		wg:      sync.WaitGroup{},
-		mx:      sync.Mutex{},
-		ch:      make(chan *Task, bufSize),
-		log:     log,
+		workers:      make([]WorkerInterface, poolSize),
+		wg:           sync.WaitGroup{},
+		mx:           sync.Mutex{},
+		workerCh:     make(chan *Task, bufSize),
+		pendingTasks: make([]*Task, bufSize),
+		hasTasks:     make(chan struct{}, 1),
+		log:          log,
 	}
 
 	for i := range poolSize {
-		pool.workers[i] = NewWorker(i, pool.ch, log, action)
+		pool.workers[i] = NewWorker(i, pool.workerCh, log, action)
 	}
 
 	return pool
@@ -97,7 +101,7 @@ func (w *Worker) Run(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			return
-		case task, opened := <-w.ch:
+		case task, opened := <-w.tasksCh:
 			if !opened {
 				return
 			}
@@ -105,22 +109,22 @@ func (w *Worker) Run(ctx context.Context) {
 			if err != nil {
 				slog.Errorf("Failed to process task (orderId: %s, userID: %s): %s", task.OrderID, task.UserID, err.Error())
 			}
-			// default:
-			// log.Debug("blocked")
-			// time.Sleep(time.Millisecond * 100)
 		}
 	}
 }
 
 // Обработка заказа
 func (wp *WorkerPool) Process(ctx context.Context, task *Task) {
+	wp.mx.Lock()
+	wp.pendingTasks = append(wp.pendingTasks, task)
+	wp.mx.Unlock()
+
 	select {
-	case <-ctx.Done():
-		return
-	case wp.ch <- task:
-		return
-		// default:
-		// wp.queue = append(wp.queue, task)
+	case wp.hasTasks <- struct{}{}:
+	default:
+		// case <-ctx.Done():
+		//	return
+		// case wp.workerCh <- task:
 		// return
 	}
 }
@@ -140,9 +144,16 @@ func (wp *WorkerPool) Start(parentCtx context.Context) {
 		}(ctx, worker, wp.log)
 	}
 
+	wp.wg.Add(1)
+
+	go func() {
+		wp.runDispatcher(ctx)
+		wp.wg.Done()
+	}()
+
 	go func() {
 		wp.wg.Wait()
-		close(wp.ch)
+		close(wp.workerCh)
 	}()
 }
 
@@ -159,4 +170,37 @@ func (wp *WorkerPool) Stop() {
 // Ожидаем когда сотрудники доделают работу
 func (wp *WorkerPool) Wait() {
 	wp.wg.Wait()
+}
+
+func (wp *WorkerPool) runDispatcher(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-wp.hasTasks:
+			wp.drainQueue(ctx)
+		}
+	}
+}
+
+func (wp *WorkerPool) drainQueue(ctx context.Context) {
+	for {
+		wp.mx.Lock()
+		defer wp.mx.Unlock()
+
+		if len(wp.pendingTasks) == 0 {
+			return
+		}
+
+		task := wp.pendingTasks[0]
+
+		select {
+		case wp.workerCh <- task:
+			wp.pendingTasks = wp.pendingTasks[1:]
+		case <-ctx.Done():
+		default:
+			return
+		}
+		wp.mx.Unlock()
+	}
 }
